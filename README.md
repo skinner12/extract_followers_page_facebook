af8dda83dbee53c66314df50692be4590323cb30
tux <mazzu83@gmail.com>
2019-12-18 23:53:15 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# EXTRACT FOLLOWERS NAME FROM FACEBOOK'S PAGE
+
+## How to see who is following you on Facebook
+
+### 1) Go to the "Settings" tab on your page
+
+![Go to the "Settings" tab on your page](https://image.businessinsider.com/5ca4cab5c6cc50577a68e323?width=700&format=jpeg&auto=webp)
+
+Click the "Settings" tab on your page.
+
+### 2) Click "People and Other Pages" on the left-hand sidebar
+
+![ Click "People and Other Pages" on the left-hand sidebar](https://image.businessinsider.com/5ca4cadb16c95831521a5c17?width=700&format=jpeg&auto=webp)
+
+Select "People and Other Pages."
+
+### 3) Toggle the menu to select "People Who Follow This Page."
+
+![Toggle the menu to select "People Who Follow This Page."](https://image.businessinsider.com/5ca4cb8416c958315700c135?width=700&format=jpeg&auto=webp)
+Select "People Who Follow This Page" from the menu.
+
+### 4)Load all page and save it to disk. Rename it to "to_extract.html"
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gocolly/colly"
+	"github.com/tealeg/xlsx"
+)
+
+//FBUser è composto dal nome e dal link del profilo
+type FBUser struct {
+	Nome string
+	Link string
+	Data string
+}
+
+func main() {
+			var file *xlsx.File
+    var sheet *xlsx.Sheet
+    var row *xlsx.Row
+    var cell *xlsx.Cell
+		var err error
+		
+	//dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	dir, err := filepath.Abs("/home/tux/code/go/extract_followers_page_facebook")
+	if err != nil {
+		panic(err)
+	}
+
+	t := &http.Transport{}
+	t.RegisterProtocol("file", http.NewFileTransport(http.Dir("/")))
+
+	c := colly.NewCollector(colly.MaxDepth(2),
+		colly.Async(true),)
+		c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 20})
+	c.WithTransport(t)
+
+	pages := []FBUser{}
+
+	c.OnHTML("tr", func(e *colly.HTMLElement) {
+		e.ForEach("a", func(_ int, elem *colly.HTMLElement) {
+			nome := elem.Text
+			link := elem.Attr("href")
+			data := e.DOM.Find(".livetimestamp").Text()
+			fmt.Println(data)
+			//fmt.Println(link, e.Text)
+			fb := FBUser{
+					Nome:    nome,
+					Link: link,
+					Data: data,
+				}
+				pages = append(pages, fb)
+		})
+		
+	})
+
+	//c.OnHTML("a", func(e *colly.HTMLElement) {
+	//	c.Visit("file://" + dir + "/html" + e.Attr("href"))
+	//})
+
+	fmt.Println("file://" + dir + "/to_extract.html")
+	c.Visit("file://" + dir + "/to_extract.html")
+	c.Wait()
+
+	    file = xlsx.NewFile()
+    sheet, err = file.AddSheet("Sheet1")
+    if err != nil {
+        fmt.Printf(err.Error())
+		}
+		
+		row = sheet.AddRow()
+    cell = row.AddCell()
+		cell.Value = "Name"
+		cell = row.AddCell()
+		cell.Value = "Added on date"
+		cell = row.AddCell()
+		cell.Value = "Link on profile"
+		
+
+
+	for i, p := range pages {
+		fmt.Printf("%d : %s il %s - %s\n", i, p.Nome, p.Data, p.Link)
+
+
+
+    row = sheet.AddRow()
+    cell = row.AddCell()
+		cell.Value = p.Nome
+		cell = row.AddCell()
+		cell.Value = p.Data
+		cell = row.AddCell()
+		cell.Value = p.Link
+    
+	}
+
+	err = file.Save("List.xlsx")
+    if err != nil {
+        fmt.Printf(err.Error())
+    }
+}
